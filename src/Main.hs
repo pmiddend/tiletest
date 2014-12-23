db02f3cff59a6df09f231541fc84ed10fc9c54f7
Philipp Middendorf <pmi@hacon.de>
2014-12-23 13:47:15 +0100

    Intermediate commit

--- src/Main.hs
+++ src/Main.hs
@@ -4,8 +4,9 @@ module Main where
 import qualified Codec.Picture       as Juicy
 import           Control.Applicative ((<$>))
 import           Control.Comonad
-import           Control.Lens        (from, (^.))
+import           Control.Lens        (from, (^.),_1,over,each)
 import           Control.Lens.TH     (makeLenses)
+import           Data.Array          (Array, array,bounds,(!))
 import           Data.List           (sort)
 import           Data.Maybe          (fromMaybe, maybeToList)
 import qualified Data.Vector         as V
@@ -17,6 +18,7 @@ import           Wrench.Color
 import           Wrench.Engine       (Picture (..), RenderPositionMode (..),
                                       wrenchPlay)
 
+{-
 data BoxedImage a = BoxedImage
   { biWidth  :: !Int
   , biHeight :: !Int
@@ -78,37 +80,68 @@ neighbour dx dy (FocusedImage bi x y)
      outOfBounds =
          x' < 0 || x' >= biWidth bi ||
          y' < 0 || y' >= biHeight bi
-
+         -}
 data TileType = Grass | Dirt
 
+data TileProto = TileProto {
+      _tileProtoType  :: TileType
+    , _tileProtoTrack :: Bool
+  }
+
+$(makeLenses ''TileProto)
+
 data Tile = Tile {
-      _tileType  :: TileType
-    , _tileTrack :: Bool
+      _tileNeighbors :: (TileType,TileType,TileType,TileType)
+    , _tileId        :: (TileType,TileType)
+    , _tileTrack     :: Bool
   }
 
 $(makeLenses ''Tile)
 
-pixelToTile :: Juicy.PixelRGB8 -> Tile
-pixelToTile (Juicy.PixelRGB8 0 120 12) = Tile Grass False
-pixelToTile (Juicy.PixelRGB8 0 120 136) = Tile Grass True
-pixelToTile (Juicy.PixelRGB8 82 73 32) = Tile Dirt False
-pixelToTile (Juicy.PixelRGB8 82 73 107) = Tile Dirt True
-pixelToTile (Juicy.PixelRGB8 _ _ _) = Tile Grass False
+type Int2Index = (Int,Int)
 
---fileToTiles :: DynamicImage -> Array (Int,Int) Tile
---fileToTiles
+type IndexAndNeighbors = (Int2Index,Int2Index,Int2Index,Int2Index)
 
-viewportSize = V2 640 480
+type TileNeighborhood = (TileProto,TileProto,TileProto,TileProto)
+
+type PixelArray = Array Int2Index Juicy.PixelRGB8
+type TileProtoArray = Array Int2Index TileProto
+type TileArray = Array Int2Index Tile
+
+imageToPixelArray :: Juicy.Image Juicy.PixelRGB8 -> PixelArray
+imageToPixelArray im = let w = Juicy.imageWidth im
+                           h = Juicy.imageHeight im
+                       in array ((0,w),(0,h)) [((x,y),Juicy.pixelAt im x y) | x <- [0..w-1], y <- [0..h-1]]
+
+pixelToTileProtoArray :: PixelArray -> TileProtoArray
+pixelToTileProtoArray = fmap pixelToTileProto
+
+generateNonBorderIndices :: (Int2Index,Int2Index) -> [IndexAndNeighbors]
+generateNonBorderIndices ((wl,wr),(hl,hr)) = [((x,y),(x+1,y),(x,y+1),(x+1,y+1)) | x <- [wl..wr-2],y <- [hl..hr-2]]
+
+protoToTile :: (TileProto,TileProto,TileProto,TileProto) -> Tile
+protoToTile (a,b,c,d) = undefined
+
+tileProtoToTileArray :: TileProtoArray -> TileArray
+tileProtoToTileArray a = array [(ix ^. _1,protoToTile (over each (a !))) | ix <- generateNonBorderIndices (bounds a)]
+
+pixelToTileProto :: Juicy.PixelRGB8 -> TileProto
+pixelToTileProto (Juicy.PixelRGB8 0 120 12) = TileProto Grass False
+pixelToTileProto (Juicy.PixelRGB8 0 120 136) = TileProto Grass True
+pixelToTileProto (Juicy.PixelRGB8 82 73 32) = TileProto Dirt False
+pixelToTileProto (Juicy.PixelRGB8 82 73 107) = TileProto Dirt True
+pixelToTileProto (Juicy.PixelRGB8{}) = TileProto Grass False
+
+-- viewportSize = V2 640 480
 
 main :: IO ()
 main = do
-  imageReadResult <- readImage "media/track.png"
-  return ()
-{-
+  imageReadResult <- Juicy.readImage "media/track.png"
+--   return ()
   case imageReadResult of
     Left errmsg -> error errmsg
     Right (Juicy.ImageRGB8 omg) -> print $ Juicy.pixelAt omg 7 7
-    Right _ -> print "oh no"-}
+    Right _ -> print "oh no"
 
   {-do
   wrenchPlay
